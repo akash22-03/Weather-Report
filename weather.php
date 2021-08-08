c4ce3d5d0479bc47298b8db3f9b32981fedc7957
akash22-03 <akashdash2203@gmail.com>
2021-08-08 09:00:11 +0530

    Using API in my webpage

---
+++ weather.php
@@ -0,0 +1,119 @@
+<?php
+
+$weather = "";
+$error = "";
+
+if (array_key_exists('city', $_GET)) {
+  $urlContents =
+    file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=" . urlencode($_GET['city']) . "&appid=d7bae2f98463488eeaa93b933f53f044");
+  $weatherArray = json_decode($urlContents, true);
+
+  if ($weatherArray['cod'] == 200) {
+    $weather = "The weather in " . $_GET['city'] . " is currently '" . $weatherArray['weather'][0]['description'] . "'. ";
+    $tempInCelcius = $weatherArray['main']['temp'] - 273;
+
+    $weather .= "The temperature is " . intval($tempInCelcius) . "&deg;C and the wind speed is " . $weatherArray['wind']['speed'] . "m/s.";
+  } else {
+    $error = "Could not find city - please try again.";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags always come first -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Weather Scraper</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+  <style type="text/css">
+    html {
+      background: url(background.jpg) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+
+    body {
+
+      background: none;
+
+    }
+
+    .container {
+
+      text-align: center;
+      margin-top: 180px;
+      width: 450px;
+      background: rgba(255, 255, 255, 0.5);
+
+      padding: 20px 20px 20px 20px;
+      border-radius: 8px;
+
+    }
+
+    input {
+
+      margin: 20px 0;
+
+    }
+
+    #weather {
+
+      margin-top: 15px;
+
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="container">
+
+    <h1>What's The Weather?</h1>
+
+
+
+    <form>
+      <fieldset class="form-group">
+        <label for="city">Enter the name of a city.</label>
+        <input type="text" class="form-control" name="city" id="city" placeholder="Eg. Cuttack, Mumbai" value="<?php
+
+                                                                                                                if (array_key_exists('city', $_GET)) {
+
+                                                                                                                  echo $_GET['city'];
+                                                                                                                }
+
+                                                                                                                ?>">
+      </fieldset>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <div id="weather"><?php
+
+                      if ($weather) {
+
+                        echo '<div class="alert alert-success" role="alert">
+  ' . $weather . '
+</div>';
+                      } else if ($error) {
+
+                        echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+                      }
+
+                      ?></div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+
+</html>
